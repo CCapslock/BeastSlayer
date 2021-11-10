5c67401935b0812ee8808240262f45f2f6ce6537
Andrey Zakharov <raven159@yandex.ru>
2021-11-10 20:28:16 +0300

    Completed move mechanics

--- Assets/Scripts/View/PlayerView.cs
+++ Assets/Scripts/View/PlayerView.cs
@@ -4,17 +4,50 @@ namespace View
 {
     public class PlayerView : MonoBehaviour
     {
+        #region Fields
+
         [SerializeField] private float _step;
         [SerializeField] private GameObject _target;
-        
+
+        private Quaternion _startPosition;
+        private bool _isRotation;
+        private float _stepDirection;
+
+        #endregion
+
+        #region Life cycle
+
+        public void Start()
+        {
+            _startPosition = transform.rotation;
+            _isRotation = false;
+        }
+
         public void Update()
         {
-            var distance = Vector3.Distance(transform.position, _target.transform.position);
-            if (Input.GetKeyDown(KeyCode.D)) transform.RotateAround(_target.transform.position, Vector3.up, _step);
-            if (Input.GetKeyDown(KeyCode.A)) transform.RotateAround(_target.transform.position, Vector3.up, _step);
-            else transform.RotateAround(_target.transform.position, Vector3.up, 0);
+            if ((Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.A)) && !_isRotation)
+            {
+                _isRotation = true;
 
-            if (Input.GetKeyDown(KeyCode.Space)) Debug.Log($"I Attack {distance}");
+                if (Input.GetKeyDown(KeyCode.D)) _stepDirection = -_step;
+                else _stepDirection = _step;
+            }
+
+            if (_isRotation)
+            {
+                var angle = Quaternion.Angle(_startPosition, transform.rotation);
+                Debug.Log(angle);
+
+                if (angle <= _step) transform.RotateAround(_target.transform.position, Vector3.up,
+                    _stepDirection * Time.deltaTime);
+                else
+                {
+                    _isRotation = false;
+                    _startPosition = transform.rotation;
+                }
+            }
         }
+
+        #endregion
     }
 }
